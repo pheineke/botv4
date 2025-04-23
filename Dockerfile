87fc2aaf76670b304b6fe372adb0629a22d8d0e9
pheineke <heineke.pe@gmail.com>
2025-04-23 16:39:54 +0200

    Dockerfile and requirements

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.10-slim
+
+WORKDIR /app
+
+COPY . /app
+
+RUN apt-get update && apt-get install -y git nano vim
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+RUN chmod +x start.sh
+
+CMD ["python3 main.py"]
--- main.py
+++ main.py
@@ -18,18 +18,39 @@ bot = commands.Bot(command_prefix='&', intents=intents, owner_ids=set(owner_id))
 
 async def load_modules():
     modules_dir = 'modules'
-    module_files = glob.glob(os.path.join(modules_dir, 'module-*.py'))
-    for module_file in module_files:
-        module_name = os.path.basename(module_file)[:-3]  # Remove '.py' extension
-        full_module_name = f"{modules_dir}.{module_name}"
-        spec = importlib.util.spec_from_file_location(full_module_name, module_file)
-        module = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(module)
-        if hasattr(module, 'setup'):
-            await bot.load_extension(full_module_name)
-        else:
-            logger.log(f'No setup function in {full_module_name}, skipping...')
-        logger.log(f'Loaded module: {full_module_name}')
+    with open(os.path.join(modules_dir, 'active.txt'), 'r') as f:
+        data = f.readlines()
+
+    modules = []
+
+    for module in data:
+        module_name = (
+            module
+            .removesuffix('\n')
+            )
+
+        modules.append(
+            (module_name, 'modules' + '.' + module_name)
+        )
+
+
+    for module, full_module_dir in modules:
+        await bot.load_extension(full_module_dir)
+        
+
+
+
+    # for module_file in module_files:
+    #     module_name = os.path.basename(module_file)[:-3]  # Remove '.py' extension
+    #     full_module_name = f"{modules_dir}.{module_name}"
+    #     spec = importlib.util.spec_from_file_location(full_module_name, module_file)
+    #     module = importlib.util.module_from_spec(spec)
+    #     spec.loader.exec_module(module)
+    #     if hasattr(module, 'setup'):
+    #         await bot.load_extension(full_module_name)
+    #     else:
+    #         logger.log(f'No setup function in {full_module_name}, skipping...')
+    #     logger.log(f'Loaded module: {full_module_name}')
     
 @bot.event
 async def on_ready():
